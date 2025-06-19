6927ea1d255a2f523833a9d519c34d2262dac254
Louis Poidevin <58215761+lpoidevin@users.noreply.github.com>
2025-06-19 20:57:00 +0200

    Move default gnome apps on flatpak

--- scripts/setup-common.sh
+++ scripts/setup-common.sh
@@ -14,11 +14,29 @@ sudo dnf upgrade -yq
 
 log "Removing unnecessary pre-installed packages..."
 sudo dnf remove -yq \
+    baobab \
     firefox \
+    evince \
+    gnome-calculator \
+    gnome-calendar \
+    gnome-characters \
+    gnome-clocks \
+    gnome-connections \
+    gnome-contacts \
+    gnome-font-viewer \
+    gnome-logs \
+    gnome-maps \
+    gnome-text-editor \
     gnome-tour \
+    gnome-weather \
     libreoffice-* \
+    loupe \
+    simple-scan \
+    snapshot \
+    malcontent-control \
     mediawriter \
-    malcontent-control
+    ptyxis \
+    yelp
 
 ./scripts/install-packages.sh common
 ./scripts/install-flatpaks.sh common
